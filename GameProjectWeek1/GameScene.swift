e61262fc9cc63238dca0b33db9844ff9c3eacb4c
cmillstein <cmillstein@fullsail.edu>
2015-08-05 21:58:03 -0400

    Initial Commit

--- GameProjectWeek1/GameScene.swift
+++ GameProjectWeek1/GameScene.swift
@@ -9,14 +9,30 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var blueTank = SKSpriteNode(imageNamed: "blueTank1.png")
+    var Timer = NSTimer()
+    var backgroundImage = SKSpriteNode(imageNamed: "metalBackground.png")
+    
+    
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+       
+    blueTank.size = CGSize(width: 100, height: 100)
+    blueTank.position = CGPoint(x: frame.width/2, y: frame.height/2)
+    blueTank.zPosition = 1.0
         
-        self.addChild(myLabel)
+    self.addChild(blueTank)
+    
+        
+        
+    Timer = NSTimer.scheduledTimerWithTimeInterval(0.5, target: self, selector: Selector("MakeEnemiesAppear"), userInfo: nil, repeats: true)
+        
+        
+    self.addChild(backgroundImage)
+        backgroundImage.position = CGPointMake(self.size.width/2, self.size.height/2)
+        
+        
+    
     }
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
@@ -25,19 +41,85 @@ class GameScene: SKScene {
         for touch in (touches as! Set<UITouch>) {
             let location = touch.locationInNode(self)
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+           var bullet = SKSpriteNode(imageNamed: "bullet.png")
+            bullet.position = blueTank.position
+            bullet.size = CGSize(width: 25, height: 25)
+            bullet.physicsBody = SKPhysicsBody(circleOfRadius: bullet.size.width/2)
+            bullet.physicsBody?.affectedByGravity = false
             
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
             
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+            self.addChild(bullet)
+        
+        
+        
+            var xCoord = CGFloat(location.x - blueTank.position.x)
+            var yCoord = CGFloat(location.y - blueTank.position.y)
             
-            sprite.runAction(SKAction.repeatActionForever(action))
             
-            self.addChild(sprite)
+            
+            let magnitude = sqrt(xCoord * xCoord + yCoord * yCoord)
+            xCoord /= magnitude
+            yCoord /= magnitude
+            
+            let vector = CGVector(dx: 13.0 * xCoord, dy: 13.0 * yCoord)
+            
+            bullet.physicsBody?.applyImpulse(vector)
+            
+            
+        
         }
     }
+    
+    func MakeEnemiesAppear(){
+        
+        let enemy = SKSpriteNode(imageNamed: "enemy.png")
+        
+        
+        let random = arc4random() % 4
+        switch random {
+        case 0:
+            enemy.position.x = 0
+            var yPosition = arc4random_uniform(UInt32(frame.size.height))
+            enemy.position.y = CGFloat(yPosition)
+            
+            self.addChild(enemy)
+            break
+        case 1:
+            enemy.position.y = 0
+            var xPosition = arc4random_uniform(UInt32(frame.size.width))
+            enemy.position.x = CGFloat(xPosition)
+            
+            self.addChild(enemy)
+            
+            break
+        case 2:
+            enemy.position.y = frame.size.height
+            var xPosition = arc4random_uniform(UInt32(frame.size.width))
+            enemy.position.y = CGFloat(xPosition)
+            
+            self.addChild(enemy)
+            
+            break
+        case 3:
+            enemy.position.x = frame.size.width
+            var yPosition = arc4random_uniform(UInt32(frame.size.height))
+            enemy.position.y = CGFloat(yPosition)
+            
+            self.addChild(enemy)
+            
+            break
+        default:
+            break
+        }
+        
+        enemy.runAction(SKAction.moveTo(blueTank.position, duration: 5.0))
+        
+    }
+    
+    
+    
+    
+    
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
